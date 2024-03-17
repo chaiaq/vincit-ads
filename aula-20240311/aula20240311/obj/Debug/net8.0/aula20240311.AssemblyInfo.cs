987416b3a8bbc1bd2a9612e46c6fd27d75b46270
chaiaq <chaiane.ey@gmail.com>
2024-03-17 10:26:52 -0300

    17/03

--- aula-20240311/aula20240311/obj/Debug/net8.0/aula20240311.AssemblyInfo.cs
+++ aula-20240311/aula20240311/obj/Debug/net8.0/aula20240311.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula20240311")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e36613e818951caf4dee1045eb8dc133351c33c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6020b472d75e13020e433cff067871d06146ca5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula20240311")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula20240311")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
